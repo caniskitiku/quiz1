ea4a3431ec9d111ee6086008e973ccc5f2c7728e
caniskitiku <1800001312@stu.iku.edu.tr>
2021-03-20 13:18:37 +0300

    Form3.php Added

---
+++ form3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+    <p>User Input: <input type="text" name="Input" placeholder="Enter Something">
+
+    </p>
+    <input type="submit" value="Post Form" >
+    </form>
+    <pre>
+    $_GET
+    <?php 
+    print_r($_GET)
+     ?>
+     $_POST
+    <?php 
+    print_r($_POST)
+     ?>
+
+    </pre>
+</body>
+</html>
